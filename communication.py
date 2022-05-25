8e6ffa2b7a0534ea888263a1212f9e1106bc30c1
StefanKauf <kaufmannstefan@outlook.com>
2022-05-25 19:09:13 +0200

    add find attack angle

--- communication.py
+++ communication.py
@@ -17,7 +17,7 @@ plot = False
 
 
 
-def createObj(type, orientation,x,y,angle, orig, tool=False, workspace=100):
+def createObj(type, orientation,x,y,angle, orig, tool=False,move=False, workspace=100):
     """Creats a obj, screw nut, lying/standing 
         Params
          --------
@@ -28,6 +28,7 @@ def createObj(type, orientation,x,y,angle, orig, tool=False, workspace=100):
         angle:         Orientation of the main axis in grad  
         orig:          Original dimensions [x,y] of the image   
         tool:          if we want to grab with tool   # true = tool, false = no tool    default False 
+        move:          if we want to move the obj with a tool, # true = move
         workspace:     work space is the overlay of the table, we need it for the implementation default 100 mm
         Returns
         --------
@@ -57,8 +58,7 @@ def createObj(type, orientation,x,y,angle, orig, tool=False, workspace=100):
         else:   # tool for nuts
             width = 42+s
             height = 20+s     
-
-    
+   
     elif type == 1 and orientation == 2:   # screws
         width = 20
         height = 50
@@ -66,9 +66,15 @@ def createObj(type, orientation,x,y,angle, orig, tool=False, workspace=100):
         width = 20
         height = 20
     
+    if move:       
+        width = 42+s
+        height = 20+s
+
     obj = np.ones((height,width))
-    if tool:
+    if tool and not move:
         obj[:,2*s:width-2*s] = 0
+
+    
 
         
     
@@ -100,7 +106,7 @@ def createObj(type, orientation,x,y,angle, orig, tool=False, workspace=100):
           
     return img[workspace:orig[1]+workspace, workspace:orig[0]+workspace]
 
-def createImg(type, orientation,x,y,angle, orig,tool = False, workspace=100):
+def createImg(type, orientation,x,y,angle, orig,tool=False,move = False, workspace=100):
     """Creats a obj, screw nut, lying/standing, can handle scalars and arrays
         Params
          --------
@@ -110,6 +116,7 @@ def createImg(type, orientation,x,y,angle, orig,tool = False, workspace=100):
         y:             Position from origin in mm - y-Position
         angle:         Orientation of the main axis in grad  
         orig:          Original dimensions [x,y] of the image 
+        move:          if we want to move the obj with a tool, # true = move
         workspace:     work space is the overlay of the table, we need it for the implementation,  default 100 mm
 
         Returns
@@ -123,14 +130,14 @@ def createImg(type, orientation,x,y,angle, orig,tool = False, workspace=100):
         img = np.zeros((orig[1], orig[0]))
 
     if np.isscalar(type):
-        img = createObj(type,orientation,x,y,angle, orig, tool, workspace)
+        img = createObj(type,orientation,x,y,angle, orig, tool,move, workspace)
     else:
         for i in range(0,np.size(type)):
-            img += createObj(type[i],orientation[i],x[i],y[i],angle[i], orig, tool, workspace)           
+            img += createObj(type[i],orientation[i],x[i],y[i],angle[i], orig, tool,move, workspace)           
     
     return img
 
-def pick(type, orientation,x,y,angle,img, workspace=100):
+def pick(type, orientation,x,y,angle,img,move=False, workspace=100):
     """ Checks if the obj can be picked
         Params
          --------
@@ -140,6 +147,7 @@ def pick(type, orientation,x,y,angle,img, workspace=100):
         y:             Position from origin in mm - y-Position
         angle:         Orientation of the main axis in grad  
         img:           Binary image with all remaining Objects 1 = object  
+        move:          if we want to move the obj with a tool, # true = move
         tool:          if we want to grab with tool   # true = tool, false = no tool                    
 
         Returns
@@ -153,7 +161,7 @@ def pick(type, orientation,x,y,angle,img, workspace=100):
     if plot:
         y_img -= workspace*2
         x_img -= workspace*2
-    img_tool = createObj(type, orientation,x,y,angle, (x_img,y_img), True)
+    img_tool = createObj(type, orientation,x,y,angle, (x_img,y_img), True, move)
 
     img_bitwise_and = cv2.bitwise_and(img, img_tool)
     if img_bitwise_and.any():
@@ -173,7 +181,7 @@ def pick(type, orientation,x,y,angle,img, workspace=100):
 
     return possible
 
-def checkPick(type, orientation,x,y,angle,orig,img = 0):
+def checkPick(type, orientation,x,y,angle,orig, img = 0):
     """ Checks if the obj can be picked works with scalars and arrays
         Params
          --------
@@ -193,7 +201,7 @@ def checkPick(type, orientation,x,y,angle,orig,img = 0):
                 
     """
     # Create reconstructed image
-    if not img < 0:
+    if img == 0:
         img = createImg(type, orientation,x,y,angle, orig)       
        
     if plot:
@@ -261,7 +269,7 @@ def nearObj(x,y,x1,y1):
 
     return near
 
-def createGroup(type, orientation,x,y,angle, area=0):
+def createSubGroup(type, orientation,x,y,angle, area=0):
     """Creats a Group of obj with are neare
         Params
          --------
@@ -287,12 +295,10 @@ def createGroup(type, orientation,x,y,angle, area=0):
     j = []            # inidizes of whitch obj that are not seperated
     g_type = np.zeros(np.size(x))     # helpful variable to declare if an obj is part of a Group
     
-    # Error handling if we get an scalar instead of an list
-    
+    # Error handling if we get an scalar instead of an list    
     if  np.size(x) == 1:
         return x, y, angle, area
-     
-    
+         
     if np.isscalar(area):                         # For the first loop
         surface = np.ones(np.size(type))   
         print('Surface',surface) 
@@ -318,7 +324,8 @@ def createGroup(type, orientation,x,y,angle, area=0):
             g_area = np.append(g_area,surface[i-1]+surface[i] )
             g_type[i] = 3 
             g_type[i-1] = 3 
-            print('Objekt erstellen')
+            if plot:
+              print('Objekt erstellen')
         else:
             j = np.append(j,int(i))
                
@@ -337,7 +344,7 @@ def createGroup(type, orientation,x,y,angle, area=0):
     print('Ende creatGroup')
     return g_x, g_y, g_angle, g_area
 
-def move(type, orientation,x,y,angle,j):
+def createGroup(type, orientation,x,y,angle,j):
     """Creats a Group of obj with are neare
         Params
          --------
@@ -378,25 +385,94 @@ def move(type, orientation,x,y,angle,j):
     
 
     # first loop
-    m_x, m_y, m_angle, m_area = createGroup(m_type,m_orientation,m_x,m_y,m_angle)    
+    m_x, m_y, m_angle, m_area = createSubGroup(m_type,m_orientation,m_x,m_y,m_angle)    
     # other loops
     for i in range(0,np.size(type)):
         print('####################### Runde :', i)
-        m_x, m_y, m_angle, m_area = createGroup(m_type,m_orientation,m_x,m_y,m_angle,m_area)
+        m_x, m_y, m_angle, m_area = createSubGroup(m_type,m_orientation,m_x,m_y,m_angle,m_area)
     
     
     
-    m_type = np.ones(np.size(m_x)) *3           # set type seperate
+    
     m_orientation = np.ones(np.size(m_x)) *2    # Orientation chanced to lying
     print('Area:', m_area)
     print('Ende Fkt')
     return m_type, m_orientation, m_x, m_y, m_angle
  
- 
+def move(type, orientation,x, y, angle, orig):
+    """Creats the attack angle
+        Params
+         --------
+        x:             Position from origin in mm - x-Position
+        y:             Position from origin in mm - y-Position
+        angle:         Orientation of the main axis in grad   
+        
+       
+        Returns
+        --------
+        
+        m_angle:       Orientation of the attack angle of the group  
+        m_type:        set type seperate    
+        error:         if there is error, 1 = no rechable Positionen found          
+                
+    """  
+   
+
+    # check if their are more than one group
+    if np.size(x) == 1:
+        return angle + 90
+
+    # important variables
+    threshold = np.inf
+    alpha = np.zeros(np.size(x))
+    error = np.zeros(np.size(x))
+
+    # creat img with all groups
+    img = createImg(type, orientation,x,y,angle, orig)
+
     
+    
+    # Corrects unwanted angles
+    for i in range (0,np.size(x)):
+        if angle[i] < 0:
+            angle[i] += 360
 
+        if angle[i] > 180:
+            angle[i] -=180
 
+    # find nearts group    
+    for count in range (0,np.size(x)):        
+        r = 0
+        z = 0
+        # first step, check for the nearest group
+        for i in range(0,np.size(x)):
+            if not count == i:    
+                r = ((x[count]-x[i])**2+(y[count]-y[i])**2)**0.5             
+                if r < threshold:
+                    j = i   # nearest group
+        
+        # second step search for the ideal attack vector
+        # creat the vektor between the two Groups
+        m_alpha = np.arctan2((y[j]-y[count]),(x[j]-x[count]))  
 
+        # check if this position is reachable      
+        x_m = r*np.cos(m_alpha)/2 + x[count]
+        y_m = r*np.sin(m_alpha)/2 + y[count] 
+        while not pick(1,2,x_m,y_m,m_alpha,img,True):  # if alpha is not poissible --> rotate alpha 
+            m_alpha += 5    
+            z += 5
+            if z > 360:
+                print('Error --> keine erreichbare Position gefunden')
+                error[count] = 1
+                break 
+           
+        alpha[count] = np.abs(m_alpha*180/np.pi)
+ 
+    m_type = np.ones(np.size(x)) *3           # set type seperate
+
+    return alpha, m_type, error
+
+  
     
 
 
@@ -406,7 +482,7 @@ def move(type, orientation,x,y,angle,j):
 # ****************************************************************************************************************************************************************
 # main
 # ****************************************************************************************************************************************************************
-
+#For Testing
 # image size
 x = 750          
 y = 500
@@ -414,34 +490,52 @@ y = 500
 type = [1,1,1,1]
 orientation = [1,2, 1, 2]
 x_obj = [25,200, 180, 180]
-y_obj = [25,200, 190, 200]
-angle = [20,80, 0, 0]
+y_obj = [25,200, 400, 200]
+angle = [0,0, 0, 20]
 orig = [x,y]
 
 img = createImg(type, orientation,x_obj,y_obj,angle, orig)
 img_tool =  createImg(type, orientation,x_obj,y_obj,angle, orig, True )
 
-# Scalar
-print(checkPick(1, 1,25,25,20,orig))  
 
-# Array
+
+# %%
+#First Part --> seperating all objetcs who can be picked
+print('##############################################################    Erster Teil')
 j = checkPick(type, orientation,x_obj,y_obj,angle,orig)
 print('Folgende Indizies sind aufgreifbar: ', j)
+cv2.imshow('alle Objekt', img+img_tool)
+img_tool =  np.zeros((orig[1], orig[0]))
+img = np.zeros((orig[1], orig[0]))
+for i in j:
+    i = int(i)
+    img += createImg(type[i], orientation[i],x_obj[i],y_obj[i],angle[i], orig )
+    img_tool += createImg(type[i], orientation[i],x_obj[i],y_obj[i],angle[i], orig, True )    
+cv2.imshow('alle Objekt welche aufgegriffen werden', img+img_tool)
 
+cv2.waitKey()
 
+# %%
+# Second Part  --> Creat Groups
 
-
-cv2.imshow('Objekt', img+img_tool)
-
-m_type, m_orientation, m_x, m_y, m_angle = move(type, orientation,x_obj,y_obj,angle,j)
-
-#cv2.waitKey()
-
+m_type, m_orientation, m_x, m_y, m_angle = createGroup(type, orientation,x_obj,y_obj,angle,j)
+print('##############################################################    Zweiter Teil')
 print('Type: ', m_type)
-print(m_orientation)
+print('Orientierung: ', m_orientation)
 print('x: ',m_x)
 print('y: ', m_y)
 print('angle: ', m_angle)
 
+# %%
+# Third Part --> Move Parts 
+
+alpha, m_type, error = move([1,1], [1,1],[0, 100], [0,100], [20,40], orig)
+print('##############################################################    Dritter Teil')
+print(alpha)
+print(m_type)
+print(error)
+
 
 # %%
+
+
