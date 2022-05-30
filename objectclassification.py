a2ca9a87987321b9434542bec48899580ac5c2cf
FabianTsch <tschohl.fabian@gmail.com>
2022-05-30 17:13:23 +0200

    add value substitution for classes

--- objectclassification.py
+++ objectclassification.py
@@ -18,6 +18,10 @@ def execute(images):
     class_predicted = [None]*len(images) 
     for i in range(0,len(images)):
         img, ps, classes = tl.predict(images[i], model, topk=2)
-        class_predicted[i] = classes[0]
-
+        # TODO: make clean
+        if classes[0] == "Muttern":
+            class_predicted[i] = 2 
+        elif classes[0] == "Schraube":
+            class_predicted[i] = 1
+    
     return class_predicted
